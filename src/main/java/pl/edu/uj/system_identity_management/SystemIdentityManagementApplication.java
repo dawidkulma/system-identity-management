09dca14f8e3191b8e1f107548999df0db7cc3ffa
Dawid Kulma <dawidkulma@gmail.com>
2022-03-02 14:07:32 +0100

    Init

---
+++ src/main/java/pl/edu/uj/system_identity_management/SystemIdentityManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.uj.system_identity_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemIdentityManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemIdentityManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/uj/system_identity_management/SystemIdentityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.uj.system_identity_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemIdentityManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
